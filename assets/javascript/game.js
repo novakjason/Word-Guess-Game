8cddb15c2f1694d6f7aa5f24eb76a7fea0644cb9
Jason Novak <novak.jason7@gmail.com>
2019-11-04 14:00:08 -0500

    Added function to update DOM with blank word for user to guess.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,51 @@
-// Create array of random words for user to guess.
+// Create array of random words for user to guess
 const animals = ["cougar", "raccoon", "alligator", "coyote", "bison", "eagle", "bear", "bobcat", "rattlesnake", "deer"];
 console.log(animals);
 
+var underscore = [];
 
-// Choose random word to display.
+// Choose random word to display
 var randomAnimal = animals[Math.floor(Math.random() * animals.length)];
 console.log(randomAnimal);
 
 
-// Select characters of the word and display an underscore for each character.
-var underscore = [];
-
-var displayUnderscore = () => {
+// Select characters of the word and display an underscore for each character
+function displayUnderscore() {
     for (var i = 0; i < randomAnimal.length; i++) {
         underscore.push('_');
     }
+    
     return underscore;
 }
-console.log(displayUnderscore());
+
+// Displays a blank word to the user in browser
+function blankWord() {
+
+    displayUnderscore();
+    document.getElementById("random-blank-word").innerHTML = underscore.join(' ');
+
+}
 
 
-// Get users guess via keyboard input.
+// Get users guess via keyboard input
 document.onkeyup = function(keyselect) {
     console.log(keyselect);
 }
 
 
 
-// Check to see if users guess is correct.
-//     -If correct push selected letter to display in place of designated underscore.
-//     -If incorrect push selected letter to display in incorrect letter array and subtract 1 guess from guesses remaining object.
+// Check to see if users guess is correct
+//     -If correct push selected letter to display in place of designated underscore
+//     -If incorrect push selected letter to display in incorrect letter array and subtract 1 guess from guesses remaining object
 
-// Empty array for incorrect letters guessed.
+// Empty array for incorrect letters guessed
 
 var wrongGuess = [];
 
-// Object for guesses remaining and number of wins.
+// Object for guesses remaining and number of wins
 
 var tallyGuess = 10;
 
 var winStreak = 0;
+
+blankWord();
